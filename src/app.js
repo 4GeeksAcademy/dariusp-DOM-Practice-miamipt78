a6e0c3c71297467a596801db291dec2898b8458e
Dabeldab <1dareios@gmail.com>
2025-04-14 18:39:55 +0000

    saved

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var x = document.getElementById("myDIV");
 x.querySelector(".random").style.background = "green";
-x.querySelector("h3,h2").style.background = "blue";
+x.querySelector("h3").style.background = "blue";
+x.querySelector("h2").style.background = "blue";
 
 var tableElm = document.getElementById("people");
 var trArray = tableElm.querySelectorAll("tr");
